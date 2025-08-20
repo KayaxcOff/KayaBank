56c3b16545b3678098033619cea291c2febf7660
Muhammet Kaya <muham123cak@gmail.com>
2025-08-20 13:13:19 +0300

    User.java
    For using user's info in other classes

---
+++ User.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class User {
+    private String username;
+    private String password;
+    private int balance;
+
+    public User(String username, String password, int balance) {
+        this.username = username;
+        this.password = password;
+        this.balance = balance;
+    }
+
+    public String getUsername() {return username;}
+    public  String getPassword() {return password;}
+    public int getBalance() {return balance;}
+
+    public void setUsername(String username) {this.username = username;}
+    public void setPassword(String password) {this.password = password;}
+    public void setBalance(int balance) {this.balance = balance;}
+}
